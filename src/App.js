1844f4547bca976ab5b39a84e1bff9391482ca7f
Vlad-Pischaeff <pischaeff@gmail.com>
2022-05-12 19:08:42 +0500

    format fix...

--- src/App.js
+++ src/App.js
@@ -9,64 +9,64 @@ import Search from './components/Search';
 import { searchUser, getToken, getFriends } from './api/Api';
 
 const App = () => {
-	const [user, setUser] = useState({});
-	const [friends, setFriends] = useState([]);
-	const [scheme, setScheme] = useState('bright_light')
-	const [searchData, setSearchData] = useState(null);
-	const [message, setMessage] = useState(null);
-	const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState({});
+  const [friends, setFriends] = useState([]);
+  const [scheme, setScheme] = useState('bright_light')
+  const [searchData, setSearchData] = useState(null);
+  const [message, setMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-	useEffect(() => {
-		bridge.subscribe(({ detail: { type, data }}) => {
-			if (type === 'VKWebAppUpdateConfig') {
-				setScheme(data.scheme)
-			}
-		});
-		getToken();
-	}, []);
+  useEffect(() => {
+    bridge.subscribe(({ detail: { type, data }}) => {
+      if (type === 'VKWebAppUpdateConfig') {
+        setScheme(data.scheme)
+      }
+    });
+    getToken();
+  }, []);
 
-	useEffect(() => {
-		if (user?.id) {
-			getFriends(user.id)
-				.then(({ items }) => items && setFriends(items))
-				.then(() => setLoading(false))
-		}
-	}, [user]);
+  useEffect(() => {
+    if (user?.id) {
+      getFriends(user.id)
+        .then(({ items }) => items && setFriends(items))
+        .then(() => setLoading(false))
+    }
+  }, [user]);
 
-	const handlerChange = e => {
+  const handlerChange = e => {
     setSearchData(e.target.value);
   }
 
-	const handlerSubmit = async e => {
+  const handlerSubmit = async e => {
     e.preventDefault();
-		setFriends([]);
-		setLoading(true);
+    setFriends([]);
+    setLoading(true);
     let userLink = await searchUser(searchData);
     if (userLink.items.length) {
       setUser(userLink.items[0]);
-			setMessage(null);
+      setMessage(null);
     } else {
-			setUser({});
+      setUser({});
       setMessage('НЕТ ТАКОГО ПОЛЬЗОВАТЕЛЯ');
-			setLoading(false);
+      setLoading(false);
     }
   }
 
-	return (
-		<ConfigProvider scheme={scheme}>
-			<AdaptivityProvider>
-				<AppRoot>
-					<View activePanel="search">
-						<Panel id="search">
-							<Search handlerChange={handlerChange} handlerSubmit={handlerSubmit} loading={loading}/>
-							<User user={user} message={message} />
-							<Friends friends={friends} />
-						</Panel>
-					</View>
-				</AppRoot>
-			</AdaptivityProvider>
-		</ConfigProvider>
-	);
+  return (
+    <ConfigProvider scheme={scheme}>
+      <AdaptivityProvider>
+        <AppRoot>
+          <View activePanel="search">
+            <Panel id="search">
+              <Search handlerChange={handlerChange} handlerSubmit={handlerSubmit} loading={loading}/>
+              <User user={user} message={message} />
+              <Friends friends={friends} />
+            </Panel>
+          </View>
+        </AppRoot>
+      </AdaptivityProvider>
+    </ConfigProvider>
+  );
 }
 
 export default App;
